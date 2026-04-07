01c2c1c95a642361ab72a038fb57161eea45322c
Zugfritze <70323844+Zugfritze@users.noreply.github.com>
2026-04-07 12:30:32 +0800

    修改

--- build.ps1
+++ build.ps1
@@ -1,74 +1,46 @@
-$LLVM_VERSION = $args[0]
-$LLVM_REPO_URL = $args[1]
+$LLVM_REPO_URL = $args[0]
+$LLVM_BRANCH = $args[1]
 
 if ([string]::IsNullOrEmpty($LLVM_REPO_URL)) {
     $LLVM_REPO_URL = "https://github.com/llvm/llvm-project.git"
 }
 
-if ([string]::IsNullOrEmpty($LLVM_VERSION)) {
-    Write-Output "Usage: $PSCommandPath <llvm-version> <llvm-repository-url>"
-    Write-Output ""
-    Write-Output "# Arguments"
-    Write-Output "  llvm-version         The name of a LLVM release branch without the 'release/' prefix"
-    Write-Output "  llvm-repository-url  The URL used to clone LLVM sources (default: https://github.com/llvm/llvm-project.git)"
-
-	exit 1
+if ([string]::IsNullOrEmpty($LLVM_BRANCH)) {
+    $LLVM_BRANCH = "main"
 }
 
-# Clone the LLVM project.
 if (-not (Test-Path -Path "llvm-project" -PathType Container)) {
-	git clone -b "release/$LLVM_VERSION" --single-branch --depth=1 "$LLVM_REPO_URL" llvm-project
+    git clone --branch "$LLVM_BRANCH" --single-branch --depth=1 "$LLVM_REPO_URL" llvm-project
 }
 
 Set-Location llvm-project
 git fetch origin
-git checkout "release/$LLVM_VERSION"
-git reset --hard origin/"release/$LLVM_VERSION"
+git checkout "$LLVM_BRANCH"
 
-# Create a directory to build the project.
 New-Item -Path "build" -Force -ItemType "directory"
 Set-Location build
 
-# Create a directory to receive the complete installation.
 New-Item -Path "install" -Force -ItemType "directory"
 
-# Adjust compilation based on the OS.
-$CMAKE_ARGUMENTS = ""
-
-# Adjust cross compilation
-$CROSS_COMPILE = ""
-
-# Run `cmake` to configure the project, using MSVC.
-$CMAKE_CXX_COMPILER="cl.exe"
-$CMAKE_C_COMPILER="cl.exe"
-$CMAKE_LINKER_TYPE="MSVC"
-
 cmake `
-  -G "Ninja" `
-  -DCMAKE_BUILD_TYPE=Release `
-  -DCMAKE_INSTALL_PREFIX=destdir `
-  -DLLVM_ENABLE_PROJECTS="clang;lld" `
-  -DLLVM_ENABLE_TERMINFO=OFF `
-  -DLLVM_ENABLE_ZLIB=OFF `
-  -DLLVM_ENABLE_LIBXML2=OFF `
-  -DLLVM_INCLUDE_DOCS=OFF `
-  -DLLVM_INCLUDE_EXAMPLES=OFF `
-  -DLLVM_INCLUDE_GO_TESTS=OFF `
-  -DLLVM_INCLUDE_TESTS=OFF `
-  -DLLVM_INCLUDE_TOOLS=ON `
-  -DLLVM_INCLUDE_UTILS=OFF `
-  -DLLVM_OPTIMIZED_TABLEGEN=ON `
-  -DLLVM_TARGETS_TO_BUILD="X86;AArch64;RISCV;WebAssembly;LoongArch" `
-  $CROSS_COMPILE `
-  $CMAKE_ARGUMENTS `
-  ../llvm
+    -G "Ninja" `
+    -DCMAKE_INSTALL_PREFIX=install `
+    -DCMAKE_C_COMPILER="clang-cl.exe" `
+    -DCMAKE_CXX_COMPILER="clang-cl.exe" `
+    -DLLVM_USE_LINKER=lld `
+    -DCMAKE_BUILD_TYPE=Release `
+    -DLLVM_TARGETS_TO_BUILD=X86 `
+    -DLLVM_ENABLE_LTO=Thin `
+    -DLLVM_ENABLE_ZLIB=OFF `
+    -DLLVM_ENABLE_LIBXML2=OFF `
+    -DLLVM_INCLUDE_DOCS=OFF `
+    -DLLVM_INCLUDE_BENCHMARKS=OFF `
+    -DLLVM_INCLUDE_EXAMPLES=OFF `
+    -DLLVM_INCLUDE_TESTS=OFF `
+    -DLLVM_INCLUDE_TOOLS=OFF `
+    -DLLVM_INCLUDE_UTILS=OFF `
+    -DLLVM_OPTIMIZED_TABLEGEN=ON `
+    ../llvm
 
-# Showtime!
 cmake --build . --config Release
-
-# Not using DESTDIR here, quote from
-# https://cmake.org/cmake/help/latest/envvar/DESTDIR.html
-# > `DESTDIR` may not be used on Windows because installation prefix
-# > usually contains a drive letter like in `C:/Program Files` which cannot
-# > be prepended with some other prefix.
 cmake --install . --strip --config Release
